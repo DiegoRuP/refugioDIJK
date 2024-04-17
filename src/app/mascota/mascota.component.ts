77b5127e3691f1e7306267724935ad560355da12
DiegoRuP <96434128+DiegoRuP@users.noreply.github.com>
2024-04-16 19:20:55 -0600

    Se importó la API al proyecto
    Se importó desde beeceptor la api con las mascotas.

--- src/app/mascota/mascota.component.ts
+++ src/app/mascota/mascota.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AdopcionesService } from '../shared/adopciones.service';
+import { AdoptaMascota } from '../interfaces/adopcion';
 
 @Component({
   selector: 'app-mascota',
@@ -8,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './mascota.component.css'
 })
 export class MascotaComponent {
+
+  array:AdoptaMascota[] = [];
+
+  constructor( public adopcionesService: AdopcionesService) {
+  }
+
+  ngOnInit(): void {
+    console.log("En este momento el componente se cargó");
+    this.recuperarDatos();
+  }
+
+  recuperarDatos(){
+    console.log("Entré");
+
+    this.adopcionesService.retornar().subscribe({
+      next: this.successRequest.bind(this),
+      error: (err) => (console.log(err))
+  });
+  }
+
+  successRequest(data:any):void{
+    console.log("Data", data);
+    this.array = data.products;
+    console.log("Array", this.array);
+  }
 
 }
--- src/app/shared/adopciones.service.ts
+++ src/app/shared/adopciones.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdopcionesService {
 
-  constructor() { }
+  urlAPI:string = "https://refugiodijk.free.beeceptor.com/"
+
+  constructor( private http: HttpClient) { }
+
+  retornar(){
+    return this.http.get(this.urlAPI).pipe(take(1));
+  }
 }
