ca866b0f752fbb03902d6f30912d066fc50551f3
JohannBalderas <al260832@edu.uaa.mx>
2024-05-05 00:21:16 -0600

    intento de pagina de adopcion

--- src/app/adopta/adopta.component.ts
+++ src/app/adopta/adopta.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AdopcionesService } from '../shared/adopciones.service';
+import { AdoptaMascota } from '../interfaces/adopcion';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-adopta',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './adopta.component.html',
   styleUrl: './adopta.component.css'
 })
-export class AdoptaComponent {
 
+export class AdoptaComponent {
+  @Input() mascota!: AdoptaMascota[];
+
+  constructor(public adopcionesService:AdopcionesService, public activatedRoute: ActivatedRoute){
+    this.activatedRoute.params.subscribe(params => {
+      // this.mascota = AdopcionesService.getUnHeroe(params['id']);
+    })
+  }
 }
