0ee342020fc01df04092abec1181cbe758a4e1a5
JohannBalderas <al260832@edu.uaa.mx>
2024-05-04 23:33:47 -0600

    le agregué routing al footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { AdoptaComponent } from './adopta/adopta.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { ReporteComponent } from './reporte/reporte.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'adopta', component: AdoptaComponent},
+    {path: 'galeria', component: GaleriaComponent},
+    {path: 'reporte', component: ReporteComponent},
+    {path: 'about', component: AboutComponent},
+];
