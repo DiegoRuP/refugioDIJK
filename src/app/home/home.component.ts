3b5ffe215ecc0cb9e3e9d5625833645f029bad3c
Ivan Francisco <al334088@edu.uaa.mx>
2024-05-04 20:08:44 -0600

    Pipe Video
    Se implemento un pipe de protección

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { VideoAdoptarPipe } from './video-adoptar.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, VideoAdoptarPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  video:string="Jv1iolzbvZg";
 }
