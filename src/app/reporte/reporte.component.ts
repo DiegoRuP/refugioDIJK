1875c3e2727d85d845716910ae4e9c869d6bc20b
Kxpra <al322970@edu.uaa.mx>
2024-05-06 12:15:48 -0600

    reportes terminado falta diseño y nombre mascota

--- src/app/reporte/reporte.component.ts
+++ src/app/reporte/reporte.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Cita {
+  nombre: string;
+  correo: string;
+  telefono: number;
+  hora: number;
+  fecha: string;
+}
 
 @Component({
   selector: 'app-reporte',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reporte.component.html',
   styleUrl: './reporte.component.css'
 })
-export class ReporteComponent {
 
+export class ReporteComponent implements OnInit{
+  citasPasadas: any[] = [];
+  citasFuturas: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const citasGuardadas = localStorage.getItem('citas');
+    if (citasGuardadas) {
+      const citas = JSON.parse(citasGuardadas) as Cita[]; // Asegurar que las citas se interpreten como tipo Cita
+      const fechaActual = new Date().toISOString().split('T')[0]; // Obtener la fecha actual en formato YYYY-MM-DD
+
+      citas.forEach(cita => {
+        if (cita.fecha < fechaActual) {
+          this.citasPasadas.push(cita);
+        } else {
+          this.citasFuturas.push(cita);
+        }
+      });
+    }
+  }
 }
