73d83f0cecc2933432679986a46d454b4e6ad12d
Ivan Francisco <al334088@edu.uaa.mx>
2024-05-05 23:48:07 -0600

    Se implemento el formulario en las mascotas
    Si

--- src/app/una-mascota/una-mascota.component.ts
+++ src/app/una-mascota/una-mascota.component.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { MascotaService } from '../shared/mascota.service';
 import { AdoptaMascota } from '../interfaces/adopcion';
+import { FormularioComponent } from '../formulario/formulario.component';
 
 @Component({
   selector: 'app-una-mascota',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormularioComponent],
   templateUrl: './una-mascota.component.html',
   styleUrl: './una-mascota.component.css'
 })
