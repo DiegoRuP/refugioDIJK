660ab5a6c1944a045ef4e4538162959a714870e2
JohannBalderas <al260832@edu.uaa.mx>
2024-05-05 23:10:15 -0600

    agregué los 4 componentes de material
    puse una tabla, unos tooltips, un paginator y un formulario para comentarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { TablaComponent } from './tabla/tabla.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+import { ComentarioComponent } from './comentario/comentario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, FooterComponent, FormularioComponent],
+  imports: [RouterOutlet, AboutComponent, FooterComponent, FormularioComponent, TablaComponent, PaginatorComponent, ComentarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
